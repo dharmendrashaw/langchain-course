ed6ef02081bfbe0f39beb431479c4ef5bb52e658
Dharmendra Shaw <dharmendrashaw41@outlook.com>
2026-05-25 21:28:01 +0530

    structured response from ai [may support with bigger models]

--- main.py
+++ main.py
@@ -7,6 +7,8 @@ from langchain.tools import tool
 from langchain_core.messages import HumanMessage
 from langchain_ollama import ChatOllama
 from langchain_tavily import TavilySearch
+from typing import List
+from pydantic import BaseModel, Field
 
 #1 Commented to use Tavily Search tool inplace of custom too `search` written by me.
 #1 from tavily import TavilyClient
@@ -27,10 +29,22 @@ from langchain_tavily import TavilySearch
 #     print(f"called tool search: Searching for {query}")
 #     return tavily.search(query=query)
 
+class Source(BaseModel):
+    """Schema for source used by the agent"""
+    url: str = Field(description="The URL of the source")
 
-llm = ChatOllama(temperature=0, model="llama3.2")
-tools = [TavilySearch]  #1 [search]
-agent = create_agent(model=llm, tools=tools)
+class AgentResponse(BaseModel):
+    """Schema for the agent response with answer and source"""
+    
+    answer: str = Field(description="The agent's response with answer")
+    sources: List[Source] = Field(default_factory=list, description="List of sources used to generate the answer")
+
+
+llm = ChatOllama(temperature=0, model="qwen3.5:2b") 
+tools = [TavilySearch()]  #1 [search]
+# Response format mostly doesn't work with small model with AgentResponse. 
+# need to test on bigger mode like 8B or greater.
+agent = create_agent(model=llm, tools=tools, response_format=AgentResponse)
 
 
 def main():
